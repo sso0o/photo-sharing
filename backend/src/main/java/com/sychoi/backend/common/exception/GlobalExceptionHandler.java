d1dcf9cc47b31bd967302ecbf4e6bee213d32891
choesoyeon <ssssso.yyyy@gmail.com>
2026-04-04 14:57:10 +0900

    .

--- backend/src/main/java/com/sychoi/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/sychoi/backend/common/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.sychoi.backend.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +14,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getStatus())
                 .body(e.getMessage());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<?> handleNotFound(NoResourceFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Not Found");
     }
 
     @ExceptionHandler(Exception.class)
