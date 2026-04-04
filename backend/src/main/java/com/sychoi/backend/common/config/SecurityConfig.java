8685baef86d1a431d5f90fa641d5d9e09af94c82
choesoyeon <ssssso.yyyy@gmail.com>
2026-04-04 20:55:20 +0900

    :lock: 경로 접근 권한 수정

--- backend/src/main/java/com/sychoi/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/sychoi/backend/common/config/SecurityConfig.java
@@ -30,7 +30,10 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
-                                "/v3/api-docs"
+                                "/v3/api-docs",
+                                "/",
+                                "/login",
+                                "/signup"
                         ).permitAll()
                         .requestMatchers("/photos/event/**").hasRole("HOST")
                         .requestMatchers("/host/**").hasRole("HOST")
